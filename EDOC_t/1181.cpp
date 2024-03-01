1b063613029a6b2db1d573ab2d42c40376cb4ffd
SoHyunA <solim73@naver.com>
2024-03-01 19:30:47 +0900

    [solved] 1181 단어정렬

--- EDOC_t/1181.cpp
+++ EDOC_t/1181.cpp
@@ -13,6 +13,7 @@ int main()
 {
     int n; cin >> n;
     vector <string> v(n);
+    /*
     int i = 0;
     while (n--) {
         string input;  cin >> input;
@@ -29,6 +30,17 @@ int main()
     sort(v.begin(), v.end(), cmp);
     for (auto iter=v.begin();iter!=v.end();iter++) {
         cout << *iter <<"\n";
+    }*/
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end(), cmp);
+    for (int i = 0; i < n; i++) {
+        //중복제거 - 정렬된 후엔 앞뒤비교만 하면 됨
+        if (i!=0 and v[i-1] == v[i])
+            continue;
+        else
+            cout << v[i]<<"\n";
     }
   
 }
